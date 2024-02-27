d0af3feed65a93662124b3efc57494e0835547e5
Kate Mashko <katika.life@gmail.com>
2024-02-27 17:20:12 +0300

    feat: add solutions

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -133,9 +133,7 @@ function makeImmutable(/* obj */) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-
-}
+function makeWord(/* lettersObject */) {}
 
 /**
  * There is a queue for tickets to a popular movie.
